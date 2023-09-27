108f2d8efc0dc6cbffa46ebb0b8d308d11fc9b26
popeflin <popeflin@outlook.com>
2023-09-27 15:19:57 +0700

    add Simulasi Cicilan

--- app/src/main/java/com/dewabrata/myapplication/MainMenu.kt
+++ app/src/main/java/com/dewabrata/myapplication/MainMenu.kt
@@ -1,8 +1,12 @@
 package com.dewabrata.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +14,11 @@ class MainMenu : AppCompatActivity() {
         setContentView(R.layout.activity_main_menu)
 
         findViewById<TextView>(R.id.txtHeader).text = "Selamat Datang ${intent.getStringExtra("username")}"
+
+
+        findViewById<CardView>(R.id.cardSimulasi).setOnClickListener(View.OnClickListener {
+            intent = Intent(this, SimulasiCicilan::class.java)
+            startActivity(intent)
+        })
     }
 }
---
+++ app/src/main/java/com/dewabrata/myapplication/SimulasiCicilan.kt
@@ -0,0 +1,11 @@
+package com.dewabrata.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SimulasiCicilan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simulasi_cicilan)
+    }
+}
