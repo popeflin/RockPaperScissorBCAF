7f5a045bd736a5def34725baf03960e7574e9647
popeflin <popeflin@outlook.com>
2023-09-29 14:38:34 +0700

    add Detail View Recycleview

--- app/src/main/java/com/dewabrata/myapplication/BeritaDetail.kt
+++ app/src/main/java/com/dewabrata/myapplication/BeritaDetail.kt
@@ -2,10 +2,24 @@ package com.dewabrata.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.dewabrata.myapplication.model.DataBerita
 
 class BeritaDetail : AppCompatActivity() {
+
+    lateinit var berita : DataBerita
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_berita_detail)
+        berita = intent.getParcelableExtra("berita")!!
+
+        Glide.with(this).load(berita.gambarBerita).into(findViewById(R.id.gambarBerita))
+        findViewById<ImageView>(R.id.gambarUser).setImageResource(berita.gambarUser)
+        findViewById<TextView>(R.id.txtJudul).text = berita.judulBerita
+        findViewById<TextView>(R.id.txtBerita).text = berita.isiBerita
+
+
     }
 }
